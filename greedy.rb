c0a35d7a4c20c67724e7f8f1029f46c7b09ba931
Martin Hradil <mhradil@redhat.com>
2017-02-25 17:56:50 +0000

    Greedy - backtrack when needed

--- greedy.rb
+++ greedy.rb
@@ -10,19 +10,43 @@ class Greedy
     path = []
     current = @start
     visited = [current].to_set
+    blacklist = []
 
-    @days.times do |day|
+    day = 0
+    blacklist[day] = Set.new
+
+    while day < @days
+      blacklist[day + 1] = Set.new
+
       if day == @days - 1
         # last day, going to @start
         visited.delete(@start)
       end
 
-      possible = @flights[day].select { |f| f.from == current and !visited.include?(f.to) }
+      possible = @flights[day].select do |f|
+        f.from == current and
+        !visited.include?(f.to) and
+        !blacklist[day].include?(f)
+      end
+
+      if possible.empty?
+        visited.delete(current)
+        last = path.pop
+        current = last.from
+
+        day -= 1
+        blacklist[day].add last
+
+        next
+      end
+
       cheapest = possible.sort_by { |f| f.price }.first
 
       path << cheapest
       current = cheapest.to
       visited.add current
+
+      day += 1
     end
 
     path
